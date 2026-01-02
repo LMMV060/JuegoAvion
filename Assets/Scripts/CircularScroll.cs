9949fb93fad23ae2b2023091dd73e30874fef4ba
LMMV060 <lmmv060@gmail.com>
2026-01-02 12:45:21 +0100

    Cambiado para que los sprites sean escalables como quieras (incluso de forma diferente)

--- Assets/Scripts/CircularScroll.cs
+++ Assets/Scripts/CircularScroll.cs
@@ -2,30 +2,44 @@ using UnityEngine;
 
 public class CircularScroll : MonoBehaviour
 {
-    //SerializeField para el siguiente y anterior fondo
     [SerializeField] private SpriteRenderer nextBackground;
     [SerializeField] private SpriteRenderer lastBackground;
-
+    private SpriteRenderer _moveBackground;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //Comprueba al jugador (No hay otro rb)
         Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
         if (rb == null) return;
 
-        Vector3 position = transform.position;
-        float height = nextBackground.size.y;
-        
-        //Si el jugador va hacia adelante
-        if (rb.linearVelocity.y > 0)
+        float direction = rb.linearVelocity.y;
+        //Esto es para las nubes
+        if (direction == 0) return;
+
+        // Fondo/Nube actual
+        SpriteRenderer current = GetComponent<SpriteRenderer>();
+
+        // Determinar qué fondo mover
+        if (direction > 0)
         {
-            position.y += height;
-            nextBackground.transform.position = position;
+            _moveBackground = nextBackground;
         }
-        //Si el jugador va hacia atrás
-        else if (rb.linearVelocity.y < 0)
+        else
         {
-            position.y -= height;
-            lastBackground.transform.position = position;
+            _moveBackground = lastBackground;
         }
+
+        Vector3 nuevaPos = _moveBackground.transform.position;
+
+        //Poner el fondo al final del fondo actual usando los bounds pillando el max (alto) y min (bajo)
+        if (direction > 0)
+        {
+            nuevaPos.y += current.bounds.max.y - _moveBackground.bounds.min.y;
+        }
+        else
+        {
+            nuevaPos.y += current.bounds.min.y - _moveBackground.bounds.max.y;
+        }
+
+        //Aplica los calculos de antes para mover el fondo
+        _moveBackground.transform.position = nuevaPos;
     }
 }
